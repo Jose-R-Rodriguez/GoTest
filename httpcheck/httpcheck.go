91a2205ded1e705d655469658d3952b9341654d8
Jose Ramon Rodriguez <jose.shamm@gmail.com>
2018-10-30 08:02:18 -0600

    moved assertions to it's own package, added httpcheck package to help with testing http requests

---
+++ httpcheck/httpcheck.go
@@ -0,0 +1,22 @@
+package httpcheck
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Jose-R-Rodriguez/Golang_Assertions/assertions"
+	"github.com/gorilla/mux"
+)
+
+// ResponseCode asserts both codes should be equal, otherwise fails the test
+func ResponseCode(t *testing.T, expected, actual int) {
+	assertions.Assert(t, assertions.FailLater, expected == actual, "Got http code %d, expected code %d\n", actual, expected)
+}
+
+// ExecuteRequest makes an http request with the router provided and returns the responseRecorder
+func ExecuteRequest(request *http.Request, router *mux.Router) (responseRecorder *httptest.ResponseRecorder) {
+	responseRecorder = httptest.NewRecorder()
+	router.ServeHTTP(responseRecorder, request)
+	return responseRecorder
+}
