dbd1051ab45c4270034bba76e17b5acc85fd841a
jose.rodriguez <jose.rodriguez@personio.de>
2019-09-15 23:50:54 +0200

    Initial version ready

--- assertions/assertions.go
+++ assertions/assertions.go
@@ -24,7 +24,7 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 // assert fails the test if the condition is false.
 func assert(tb testing.TB, exitOnFail, condition bool, msg string, v ...interface{}) {
 	if !condition {
-		_, file, line, _ := runtime.Caller(1)
+		_, file, line, _ := runtime.Caller(2)
 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
 		failAssertion(tb, exitOnFail)
 	}
@@ -43,7 +43,7 @@ func OkNow(tb testing.TB, err error) {
 // ok fails the test if an err is not nil.
 func ok(tb testing.TB, exitOnFail bool, err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
+		_, file, line, _ := runtime.Caller(2)
 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 		// fmt.Printf("\033%s:%d: unexpected error: %s\033\n", filepath.Base(file), line, err.Error())
 		failAssertion(tb, exitOnFail)
@@ -63,7 +63,7 @@ func EqualsNow(tb testing.TB, exp, act interface{}) {
 // equals fails the test if exp is not equal to act.
 func equals(tb testing.TB, exitOnFail bool, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
+		_, file, line, _ := runtime.Caller(2)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
 		failAssertion(tb, exitOnFail)
 	}
--- assertions/example_assertions_test.go
+++ assertions/example_assertions_test.go
@@ -8,27 +8,27 @@ import (
 
 func ExampleOk() {
 	assertions.Ok(&mockTB{}, errors.New("example error"))
-	// Output: [31massertions.go:35: unexpected error: example error[39m
+	// Output: [31mexample_assertions_test.go:10: unexpected error: example error[39m
 }
 
 func ExampleOkNow() {
 	assertions.OkNow(&mockTB{}, errors.New("example error"))
-	// Output: [31massertions.go:40: unexpected error: example error[39m
+	// Output: [31mexample_assertions_test.go:15: unexpected error: example error[39m
 }
 
 func ExampleAssert() {
 	assertions.Assert(&mockTB{}, 3 == 4, "custom message %s%s", "custom str 2", "custom strings")
-	// Output: [31massertions.go:21: custom message custom str 2custom strings[39m
+	// Output: [31mexample_assertions_test.go:20: custom message custom str 2custom strings[39m
 }
 
 func ExampleAssertNow() {
 	assertions.AssertNow(&mockTB{}, 3 == 4, "custom message %s%s", "custom str 2", "custom strings")
-	// Output: [31massertions.go:16: custom message custom str 2custom strings[39m
+	// Output: [31mexample_assertions_test.go:25: custom message custom str 2custom strings[39m
 }
 
 func ExampleEquals_int() {
 	assertions.Equals(&mockTB{}, 44, 23)
-	/* Output: [31massertions.go:55:
+	/* Output: [31mexample_assertions_test.go:30:
 
 	exp: 44
 
@@ -40,7 +40,7 @@ func ExampleEquals_map() {
 	map1 := map[string]int{"dsds": 33, "lol": 420}
 	map2 := map[string]int{"dsds": 3, "ll": 420}
 	assertions.Equals(&mockTB{}, map1, map2)
-	/* Output: [31massertions.go:55:
+	/* Output: [31mexample_assertions_test.go:42:
 
 	exp: map[string]int{"dsds":33, "lol":420}
 
